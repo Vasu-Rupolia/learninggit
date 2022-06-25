02b6ec44e55b3fa8f75d7063b673a10c76970031
Vasu <rupoliavasu2@gmail.com>
2022-06-25 12:55:20 +0530

    test

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<style>
+			.container{
+				width: 90%;
+				height:auto;
+				margin:auto;
+			}
+			.boxes{
+				width:47%;
+				height: 40%;
+				display:inline-block;
+				border:1px solid grey;
+			}
+			#box1{
+				background-color:blue;
+			}
+			#box2{
+				background-color:yellow;
+			}
+			#box3{
+				background-color:green;
+			}
+			#box4{
+				background-color:red;
+			}
+		</style>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="boxes" id="box1"></div>
+			<div class="boxes" id="box2"></div>
+			<div class="boxes" id="box3"></div>
+			<div class="boxes" id="box4"></div>	
+		</div>
+	</body>	
+</html>
